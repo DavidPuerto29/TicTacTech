f95f52b2a0c1de25b3dea236ab8a0e6dca4ee27f
DavidPuerto <tutos6782@gmail.com>
2025-10-15 11:57:08 +0200

    Initial commit

---
+++ src/main/java/com/davidpuertocuenca/TicTacTech/TicTacTechApplication.java
@@ -0,0 +1,13 @@
+package com.davidpuertocuenca.TicTacTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacTechApplication.class, args);
+	}
+
+}
