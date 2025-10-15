474711ec8bbe421212b2bf475b7a7782091200a1
DavidPuerto <tutos6782@gmail.com>
2025-10-15 12:04:48 +0200

    creating user class.

---
+++ src/main/java/com/davidpuertocuenca/TicTacTech/model/User.java
@@ -0,0 +1,21 @@
+package com.davidpuertocuenca.TicTacTech.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private Long gamesWon;
+    private Long gamesLost;
+    //Juegos totales suma de (gamesWon + gamesLost)
+}
